6053e1d6294eced66872b0f53721ed4698b85ae1
Narendra <nkadiri09@gmail.com>
2021-02-12 06:48:14 -0500

    Added inMemoryAuthentication authentication and configured multiple users

--- src/main/java/com/naren/config/ProjectSecurityConfig.java
+++ src/main/java/com/naren/config/ProjectSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.naren.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -39,5 +41,12 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 antMatchers("/contact").permitAll().
                 and().formLogin().and().httpBasic();
     }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").
+                and().withUser("naren").password("Test@123").authorities("read").
+                and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
 
 }
