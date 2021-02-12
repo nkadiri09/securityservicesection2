495323c7c2050e21d3a3a48d1637c23c02559950
Narendra <nkadiri09@gmail.com>
2021-02-12 07:02:35 -0500

    Added InMemoryUserDetailsManager Accepts UserDetails

--- src/main/java/com/naren/config/ProjectSecurityConfig.java
+++ src/main/java/com/naren/config/ProjectSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.naren.config;
 
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -42,11 +48,31 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 and().formLogin().and().httpBasic();
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    // inMemoryAuthentication for configuring user inMemory
+/*    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").
                 and().withUser("naren").password("Test@123").authorities("read").
                 and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }*/
+
+    // Configuring all user in InMemoryUserDetailsManager using UserDetails class
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.withUsername("Naren").password("Test@321").authorities("admin").build();
+        UserDetails userDetails1 = User.withUsername("kadiri").password("Test@123").authorities("read").build();
+        userDetailsManager.createUser(userDetails);
+        userDetailsManager.createUser(userDetails1);
+        auth.userDetailsService(userDetailsManager);
     }
+
+
+    // Need to have PasswordEncode to intercept and validate username, and password.
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
 
 
 }
